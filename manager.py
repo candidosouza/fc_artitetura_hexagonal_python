9503b1aad3507344a88a4564755148b49105ae41
Candido Souza <candidosouzza@gmail.com>
2021-08-11 07:42:25 -0300

    Alteração de configurações

--- manager.py
+++ manager.py
@@ -1,11 +1,25 @@
 import sys
+import os
+import dotenv
 from django.conf import settings
 from django.urls import path
+from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+DEBUG = True if os.environ['DEBUG'] == "True" else False
+SECRET_KEY = os.environ['DEBUG']
+ALLOWED_HOSTS = ['*']
+
 settings.configure(
-    DEBUG=True,
-    SECRET_KEY='thisismysecretkey',
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
     ROOT_URLCONF=__name__,
     MIDDLEWARE_CLASSES=(
         'django.middleware.common.CommonMiddleware',
@@ -22,6 +36,8 @@ def index(request):
 urlpatterns = [
     path('', index, name='index'),
 ]
+
+application = get_wsgi_application()
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
