909a90593235ff876c6bc2e33f88bd26c4d88c4c
Candido Souza <candidosouzza@gmail.com>
2021-12-11 15:14:00 -0300

    :bug: Correção na validação

--- application/product.py
+++ application/product.py
@@ -1,4 +1,5 @@
 import uuid
+#https://github.com/CSenshi/Validator
 from validator import validate, validate_many
 
 class ProductMeta(type):
@@ -10,60 +11,62 @@ class ProductMeta(type):
             hasattr(subclass, 'id') and callable(subclass.id) and
             hasattr(subclass, 'name') and callable(subclass.name) and
             hasattr(subclass, 'status') and callable(subclass.status) and
-            hasattr(subclass, 'price') and callable(subclass.price)and
+            hasattr(subclass, 'price') and callable(subclass.price) and
             hasattr(subclass, 'is_valid') and callable(subclass.is_valid) and
             hasattr(subclass, 'enabled') and callable(subclass.enabled) and
             hasattr(subclass, 'disabled') and callable(subclass.disabled) or
             NotImplemented
         )
 
+
 class ProductInterface(metaclass=ProductMeta):
 
     def __str__(self) -> str:
-        return f'ProductInterface'
+        return 'ProductInterface'
 
     def id(self):
-        pass
+        pass  # this function is empty
 
     def name(self) -> str:
-        pass
+        pass  # this function is empty
 
     def status(self) -> str:
-        pass
+        pass  # this function is empty
 
     def price(self) -> str:
-        pass
+        pass  # this function is empty
 
     def is_valid(self) -> bool:
-        pass
+        pass  # this function is empty
 
     def enabled(self) -> None:
-        pass
+        pass  # this function is empty
 
     def disabled(self) -> None:
-        pass
+        pass  # this function is empty
+
 
 DISABLED = "disabled"
 ENABLED = "enable"
+
 
 class Product(ProductInterface):
     __slots__ = ['__id', '__name', '__status', '__price']
 
     rules = {
-        "__id": "required|uuidv4",
         "__name": "required|min:3",
         "__status": "required|min:6|max:9",
-        "__price": "required|float"
+        "__price": "required"
     }
 
     def __init__(self, name, status, price):
-       self.__id = uuid.uuid4()
-       self.__name = name
-       self.__status = status
-       self.__price = price
+        self.__id = uuid.uuid4()
+        self.__name = name
+        self.__status = status
+        self.__price = price
 
     def __str__(self) -> str:
-        return f'Product'
+        return 'Product'
 
     @property
     def id(self):
@@ -81,34 +84,37 @@ class Product(ProductInterface):
     def price(self):
         return self.__price
 
+    def data(self):
+        return {
+            "__name": self.name,
+            "__status": self.status,
+            "__price": self.price
+        }
+
     def is_valid(self) -> bool:
         if self.status == 0:
             self.__status = DISABLED
-
-        if self.status != ENABLED and self.status != DISABLED:
+        if self.status not in [ENABLED, DISABLED]:
             raise ValueError("The status must be enable or disabled")
-
         if self.price < 0:
             raise ValueError("The price must be greater or equal zero")
-
-        result, validated_data, errors = validate(Product, self.rules, return_info=True)
-
+        result, validated_data, errors = validate(
+            self.data(), self.rules, return_info=True)
         if not result:
             return errors
-
         return True
 
     def enabled(self) -> None:
         if self.__price > 0:
             self.__status = ENABLED
             return None
-        raise ValueError("The price must be greater than zero to enable the product")
+        raise ValueError(
+            "The price must be greater than zero to enable the product")
 
     def disabled(self) -> None:
         if self.__price == 0:
             self.__status = DISABLED
             return None
-        raise ValueError("The price must be zero in order to have the product disabled")
+        raise ValueError(
+            "The price must be zero in order to have the product disabled")
 
-
-print(issubclass(Product, ProductInterface))
